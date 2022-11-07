edf8db562611ee1e7c9fc79dd8e77b8ce6479082
Dhiraj Bag <dhirajbag.db@gmail.com>
2022-11-08 00:19:25 +0530

    implemented is_closed() and interesction()

--- Pattern.py
+++ Pattern.py
@@ -1,4 +1,5 @@
 import copy
+
 class Pattern:
     def __init__(self, itemset: set, object: dict):
         self.itemset = itemset
@@ -13,6 +14,41 @@ class Pattern:
 
     def add_item(self, item):
         self.itemset.add(item)
+    
+    def get_object(self) -> dict:
+        return copy.deepcopy(self.object)
+    
+    def get_itemset(self) -> set:
+        return self.itemset
+    
+    def size(self) -> int:
+        return len(self.itemset)
+    
+    def is_closed(self, pattern_list) -> bool:
+        """Checks whether the pattern is closed within pattern_list"""
+        search_space = [pat for pat in pattern_list if pat.get_object()==self.get_object() and pat.size() > self.size()]
+    
+        for pat in search_space:
+            if self.get_itemset().issubset(pat.get_itemset()):
+                return False
+                
+        return True
+    
+    def intersection(self, other):
+        """Returns the interesction with the given pattern"""
+        itemset = self.get_itemset().intersection(other.get_itemset())
+        new_object = self.get_object()
+        other_object = other.get_object()
+        for key in other_object:
+            if key in new_object:
+                for e in other_object[key]:
+                    if e not in new_object[key]:
+                        new_object[key].append(e)
+            else:
+                new_object[key] = other_object[key]
+
+        return Pattern(itemset, new_object)
+
 
     def __str__(self): 
         return "Pattern(\n\titemset: "+str(self.itemset)+"\n\tobject: "+str(self.object)+"\n)"
@@ -24,4 +60,4 @@ if __name__ == "__main__":
 
     p2.object["AP"] = [1]
 
-    print(p1)
+    print(p1.get_itemset())
