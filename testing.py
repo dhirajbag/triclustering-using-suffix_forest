ca7a27a07db9c434528183f6f4bf68b3491f7cee
Dhiraj Bag <dhirajbag.db@gmail.com>
2022-11-08 20:35:16 +0530

    Ran the algorithms on sample SFDs. Outputs:  - forest.json : the forest in the form of a JSON object. - forest.png : a diagramatic representation of the suffix forest. - rule_AR_E.json : Exact association rules. - rule_AR_SB : Approximate association rules. - rule_AR_PB : Proper base approximate association rules.

--- testing.py
+++ testing.py
@@ -1,15 +1,33 @@
 from suffix_forest import build_sufix_forest
 from frequent_patterns import get_FCPs
 from demodata import SFD_mdm, SFD_om
+from association_rules import Rule
 
-
-forest = build_sufix_forest({"MDM": SFD_mdm})
+forest = build_sufix_forest({"MDM": SFD_mdm, "OM": SFD_om})
 FCPs = get_FCPs(forest)
-print(len(FCPs))
-for pat in FCPs:
-    print(pat)
+print("Total "+str(len(FCPs))+" FCP geerated.")
+# for pat in FCPs:
+#     print(pat)
+
+from generators import get_generators
+
+GEN = get_generators(FCPs)
+
+# for gen in GEN:
+#     print("generator: " + str(gen[0]))
+#     print("closure: " + str(gen[1]))
+
+rules = Rule.generate_rules(GEN, FCPs)
+
+from json import dumps
+
+with open("forest.json", "w") as outputfile:
+    outputfile.write(dumps(forest, indent=2))
+
+for key in list(rules.keys()):
+    print("Total "+str(len(rules[key]))+" "+str(key)+" rules generated.")
+    with open("rule_"+str(key)+".json", "w") as outputfile:
+        outputfile.write(dumps([rule.toJSON() for rule in rules[key]], indent=2))
 
 from printing_util import generate_forest_image
 generate_forest_image(forest, "forest.png")
-
-
