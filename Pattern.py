3b7312ce9ff701037ba77b4d8ba0f780b531735b
Dhiraj Bag <dhirajbag.db@gmail.com>
2022-11-22 23:48:44 +0530

    - added has_same_object() which internally calls is_object_subset() in both ways. - fixed __eq__(): added has_same_object() - added merge_leaf() - fixed is_closed(): which will call has_same_object() for comparision

--- Pattern.py
+++ Pattern.py
@@ -10,7 +10,7 @@ class Pattern:
             return False
         if self.itemset != __o.itemset:
             return False
-        return self.object == __o.object
+        return self.has_same_object(__o)
 
     def add_item(self, item):
         self.itemset.add(item)
@@ -29,7 +29,7 @@ class Pattern:
     
     def is_closed(self, pattern_list) -> bool:
         """Checks whether the pattern is closed within pattern_list"""
-        search_space = [pat for pat in pattern_list if pat.get_object()==self.get_object() and pat.size() > self.size()]
+        search_space = [pat for pat in pattern_list if pat.size() > self.size() and self.has_same_object(pat)]
     
         for pat in search_space:
             if self.get_itemset().issubset(pat.get_itemset()):
@@ -52,6 +52,29 @@ class Pattern:
 
         return Pattern(itemset, new_object)
 
+    def merge_leaf(self, leaf: dict):
+        for state in list(leaf.keys()):
+            if state not in self.object.keys():
+                self.object[state] = copy.deepcopy(leaf[state])
+            else:
+                for type in leaf[state]:
+                    if type not in self.object[state]:
+                        self.object[state].append(type)
+    
+    def is_object_subset(self, object1, object2):
+        """Returns whether object1 is a subset of object2"""
+
+        for state in list(object1.keys()):
+            if state not in object2.keys():
+                return False
+            else:
+                for type in object1[state]:
+                    if type not in object2[state]:
+                        False
+        return True
+
+    def has_same_object(self, other):
+        return self.is_object_subset(self.object, other.get_object()) and self.is_object_subset(other.get_object(), self.object)
 
     def __str__(self): 
         return "Pattern(\n\titemset: "+str(self.itemset)+"\n\tobject: "+str(self.object)+"\n)"
