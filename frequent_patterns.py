4f06ee93907307bb9fffc66d2a02690416be183c
Dhiraj Bag <dhirajbag.db@gmail.com>
2022-11-22 23:41:45 +0530

    form_patterns() - cannot directly return upon encounter with leaf != None because children may still exist.
    get_all_patterns() - Merged same itemsets but having different leaves
    
    get_FCPs() - Fixed duplicate itemsets problem

--- frequent_patterns.py
+++ frequent_patterns.py
@@ -1,7 +1,3 @@
-# Extracts Frequent Closed Patterns (FCPs)
-# Input: h_tree
-# Output: FCP
-
 import copy
 from Pattern import Pattern
 
@@ -11,34 +7,52 @@ def get_FCPs(h_tree):
 
     all_patterns = get_all_Patterns(h_tree)
     FCP = [ pattern for pattern in all_patterns if pattern.is_closed(all_patterns)]
-    
+
+    # Handled the case when the intersection itemset is already present in FCP
     new_may_exist = True
     while new_may_exist:
         NFCP = []
+        modify_patterns = [] 
         for pat1 in FCP:
             for pat2 in FCP:
-                if pat1 != pat2:
+                if pat1.get_itemset() != pat2.get_itemset():
                     pat = pat1.intersection(pat2)
-                    if pat.size() > 0 and pat not in FCP and pat not in NFCP:
-                        NFCP.append(pat)
-        
-        if len(NFCP) == 0:
+
+                    if(pat.size()==0):
+                        continue
+                    # Finding pat in FCP
+                    found_in_fcp = False
+                    for i in range(len(FCP)):
+                        if(FCP[i].get_itemset()==pat.get_itemset()):
+                            found_in_fcp = True
+                            # If the leaf object of pat is not already included,
+                            # then, noting that down in modifY_patterns
+                            if(not pat.is_object_subset(pat.get_object(), FCP[i].get_object())):
+                                modify_patterns.append({"idx": i, "leaf": pat.get_object()})
+                            break
+                    
+                    if not found_in_fcp:
+                        # Finding in NFCP
+                        found_in_nfcp = False
+                        for i in range(len(NFCP)):
+                            if(NFCP[i].get_itemset()==pat.get_itemset()):
+                                NFCP[i].merge_leaf(pat.get_object())
+                                found_in_nfcp = True
+                                break
+                        if not found_in_nfcp:
+                            NFCP.append(pat)
+                 
+        if len(NFCP) + len(modify_patterns) == 0:
             new_may_exist = False
         else:
+            # Patching the previously noted leaves to corresponding FCP
+            for patch in modify_patterns:
+                FCP[patch["idx"]].merge_leaf(patch["leaf"])
             for pat in NFCP:
                 FCP.append(pat)
     
     return FCP
 
-# def are_same(pattern1, pattern2):
-#     """Checks whether two patterns are same along with their objects"""
-#     if len(pattern1["itemset"]) != len(pattern2["itemset"]):
-#         return False
-#     pass
-
-
-# def get_insersection(pattern1, pattern2):
-#     """Returns the interesction of two patterns """
 
 
 def get_all_Patterns(h_tree) -> list:
@@ -48,20 +62,25 @@ def get_all_Patterns(h_tree) -> list:
         some_patterns = form_patterns(h_tree[key])
         for pattern in some_patterns:
             if pattern.size() > 0:
-                all_patterns.append(pattern)
+                # Case when same itemset already exists but with a different leaf
+                # Merging both the leaves in that case
+                already_present = False
+                for i in range(len(all_patterns)):
+                    if(all_patterns[i].get_itemset()==pattern.get_itemset()):
+                        already_present = True
+                        all_patterns[i].merge_leaf(pattern.get_object())
+                        break
 
+                if not already_present:
+                    all_patterns.append(pattern)           
     return all_patterns
 
 def form_patterns(h_node):
     """Returns all possible item sets along with their object lists
      by traversing the input h_node """
     
-    if h_node["leaf"] != None:
-        return [
-            Pattern(set(), h_node["leaf"]),
-            Pattern(set([h_node["item"]]), h_node["leaf"])
-        ]
-
+    # CANNOT directly return if we encounter a leaf
+    # The the current node may have both a leaf and children
     result = []
 
     for child in h_node["children"]:
@@ -71,23 +90,13 @@ def form_patterns(h_node):
             new_pattern.add_item(h_node["item"])
             result.append(pattern)
             result.append(new_pattern)
+    
+    if h_node["leaf"] != None:
+        result.append(Pattern(set(), h_node["leaf"]))
+        result.append(Pattern(set([h_node["item"]]), h_node["leaf"]))
 
     return result
-
-# def is_closed(pattern, all_patterns):
-#     search_space = [pat for pat in all_patterns if pat["object"]==pattern["object"] and len(pat["itemset"]) > len(pattern)]
     
-#     for pat in search_space:
-#         if is_subset(pattern["itemset"], pat["itemset"]):
-#             return False
-            
-#     return True
-
-# def is_subset(A, B):
-#     """Checks whether A is a subset of B"""
-#     return all(x in B for x in A)
-    
-
 
 if __name__ == "__main__":
     h_tree = {
