8b72efeacb43cb4ec499c1c3d904293dd68496bb
Dhiraj Bag <dhirajbag.db@gmail.com>
2022-11-22 23:55:46 +0530

    Ran testing.py and updated outputs

--- testing.py
+++ testing.py
@@ -5,17 +5,15 @@ from association_rules import Rule
 
 forest = build_sufix_forest({"MDM": SFD_mdm, "OM": SFD_om})
 FCPs = get_FCPs(forest)
-print("Total "+str(len(FCPs))+" FCP geerated.")
-# for pat in FCPs:
-#     print(pat)
+print("Total: "+str(len(FCPs))+" FCP geerated.")
 
 from generators import get_generators
 
 GEN = get_generators(FCPs)
 
 # for gen in GEN:
-#     print("generator: " + str(gen[0]))
-#     print("closure: " + str(gen[1]))
+#     print("gen: " + str(gen[0].get_itemset()))
+#     print("\tclos: " + str(gen[1].get_itemset()))
 
 rules = Rule.generate_rules(GEN, FCPs)
 
@@ -27,7 +25,7 @@ with open("forest.json", "w") as outputfile:
 for key in list(rules.keys()):
     print("Total "+str(len(rules[key]))+" "+str(key)+" rules generated.")
     with open("rule_"+str(key)+".json", "w") as outputfile:
-        outputfile.write(dumps([rule.toJSON() for rule in rules[key]], indent=2))
+        outputfile.write(dumps([rule.toJSON(True) for rule in rules[key]], indent=2))
 
 from printing_util import generate_forest_image
 generate_forest_image(forest, "forest.png")
