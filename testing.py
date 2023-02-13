8f77b8c0076a59b2ea3d5389037ce095ebc17c25
Dhiraj Bag <dhirajbag.db@gmail.com>
2023-02-13 15:35:25 +0530

    Removed used groceries dataset as input, removed unnecessary JSON and png outputs, created CSV files as outputs

--- testing.py
+++ testing.py
@@ -1,33 +1,42 @@
 from suffix_forest import build_sufix_forest
 from frequent_patterns import get_FCPs
-from demodata import SFD_mdm, SFD_om, SFD_1
+from generators import get_generators
 from association_rules import Rule
-from tricluster import get_clusters
+from tricluster import write_triclusters_to_csv
+from preprocessing import get_number_table_sfd_list_min_sup_count_datatset_size
+from json import dumps
+import pandas as pd
+from demodata import SFD_mdm, SFD_om, SFD_1
+
+# TODO: [Done] incorporate metrics such as lift in the association rules
+# TODO: [Done] store SFDs in csv and then read
+# TODO: [Done] use actual datasets (nominal/binary/ordinal)
+# TODO: [Done] Preprocessing & SFD creation (item mapping)
+# TODO: [Done] Decode the item numbers and use original values in the generated outputs
+# TODO: [Done] association rules in csv
+# TODO: [Done partially] store tri-clusters in csv
+# TODO: create python notebook file and execute on google colab
 
 # forest = build_sufix_forest({"D1": SFD_1})
-forest = build_sufix_forest({"MDM": SFD_mdm, "OM": SFD_om})
-FCPs = get_FCPs(forest, min_support_count=3)
-triclusters = get_clusters(FCPs, min_support_count=3, min_size=2)
+(number_table, sfd_list, min_sup_count, dataset_size) = get_number_table_sfd_list_min_sup_count_datatset_size("./input/groceries_dataset.csv", min_sup_percentage=6)
+# forest = build_sufix_forest({"MDM": SFD_mdm, "OM": SFD_om})
+print(f"Dataset has {dataset_size} rows")
+print(f"Minimum support count used for single items: {min_sup_count}")
 
-print("Total: "+str(len(FCPs))+" FCP generated.")
+forest = build_sufix_forest(sfd_list)
 
-from generators import get_generators
+min_sup_count_itemsets = 1
+print(f"Using minimum support count = {min_sup_count_itemsets} for finding FCPs")
+
+FCPs = get_FCPs(forest, min_support_count = min_sup_count_itemsets)
 GEN = get_generators(FCPs)
 
-rules = Rule.generate_rules(GEN, FCPs)
+output_file_dir = "./output/"
 
-from json import dumps
+item_name = dict()
+for (name, number) in number_table.items():
+    item_name[number] = name
 
-with open("forest.json", "w") as outputfile:
-    outputfile.write(dumps(forest, indent=2))
+write_triclusters_to_csv(output_file_dir, FCPs, item_name, dataset_size, min_support_count=min_sup_count_itemsets, min_size=2)
 
-with open("triclusters.json", "w") as outputfile:
-    outputfile.write(dumps(triclusters, indent=2))
-
-for key in list(rules.keys()):
-    print("Total "+str(len(rules[key]))+" "+str(key)+" rules generated.")
-    with open("rule_"+str(key)+".json", "w") as outputfile:
-        outputfile.write(dumps([rule.toJSON(True) for rule in rules[key]], indent=2))
-
-from printing_util import generate_forest_image
-generate_forest_image(forest, "forest.png")
+Rule.generate_csv(GEN, FCPs, item_name, dataset_size, output_file_dir, min_confidence=0.0)
