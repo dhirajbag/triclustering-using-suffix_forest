7220831fc9cad8ec95526af5e947daf7874da28f
Dhiraj Bag <dhirajbag.db@gmail.com>
2023-02-13 15:31:23 +0530

    implemented 1. get_numberTable_sfdList_supCount_datasetSize() that takes in the input csv filepath
    2. get_dataframe() that preprocesses the dataset and transforms into usable form.
    
    3. form_number_table() that creates the number_table (item to item number mapping) from the input dataset and min_sup_percentage
    
    4. get_sfd_list() that groups the input dataset according to some column and forms multiple SFDs.

--- preprocessing.py
+++ preprocessing.py
@@ -2,28 +2,27 @@ import pandas as pd
 import math
 
 
-def get_dataframe(filepath):
+def get_dataframe(filepath: str):
     df = pd.read_csv(filepath)
     grouped = df.groupby(["date", "member_number"]).agg({"item_name": lambda x: ",".join(x)})
     grouped = grouped.rename(columns={'item_name': 'item_list'})
     grouped['order_id'] = range(1, len(grouped) + 1)
     result = grouped.reset_index()
     result = result[['order_id', 'date', 'member_number', 'item_list']]
-    result['month'] = pd.to_datetime(result['date']).dt.month
-    result.to_csv("./output/result.csv")
+    # result['month'] = pd.to_datetime(result['date']).dt.month
+    result['month'] = [ date.split('-')[1] for date in result['date']]
+    result.to_csv("./output/preprocessed_datset.csv")
     return result
 
 
-def form_number_table(result, min_support_percentage = 5):
+def form_number_table(result, min_support_count):
     support = dict()
     for i in range(len(result)):
-        for item in result.loc[i,'item_list'].split(','):
+        for item in set(result.loc[i,'item_list'].split(',')):
             if item not in support:
                 support[item] = 1
             else:
                 support[item] += 1
-
-    min_support_count = math.floor(len(result)*min_support_percentage/100)
 
     items = [ (item, count) for (item, count) in support.items() if count >= min_support_count]
     items = sorted(items, key = lambda x: x[1])
@@ -35,7 +34,7 @@ def form_number_table(result, min_support_percentage = 5):
     return number_table
 
 
-def get_sfd_list(result, number_table):
+def get_sfd_list_from_processed_dataframe(result, number_table: dict):
     SFD_list = dict()
 
     grouped = result.groupby(by='month')
@@ -44,7 +43,7 @@ def get_sfd_list(result, number_table):
         SFD = dict()
         for i in data.index:
             itemset = list()
-            item_list = data['item_list'][i].split(',')
+            item_list = set(data['item_list'][i].split(','))
             for (item, item_number) in number_table.items():
                 if item in item_list:
                     itemset.append(item_number)
@@ -53,9 +52,28 @@ def get_sfd_list(result, number_table):
 
     return SFD_list
 
+def get_single_sfd(result, number_table):
+    SFD = dict()
+    for i in result.index:
+        itemset = list()
+        item_list = set(result['item_list'][i].split(','))
+        for (item, item_number) in number_table.items():
+            if item in item_list:
+               itemset.append(item_number)
+        SFD['T'+str(result['order_id'][i])] = itemset
+    
+    return SFD
+
+
+def get_number_table_sfd_list_min_sup_count_datatset_size(input_file_path: str, min_sup_percentage = 5):
+    df = get_dataframe(input_file_path)
+    min_sup_count = math.ceil((len(df)*min_sup_percentage)/100)
+    number_table = form_number_table(df, min_sup_count)
+    sfd_list = get_sfd_list_from_processed_dataframe(df, number_table)
+    return (number_table, sfd_list, min_sup_count, len(df))
+
 
 if __name__ == '__main__':
-    df = get_dataframe("./input/groceries_dataset.csv")
-    number_table = form_number_table(df, 5)
-    sfd_list = get_sfd_list(df, number_table)
+    (number_table, sfd_list, dataset_size) = get_number_table_sfd_list_min_sup_count_datatset_size("./input/groceries_dataset.csv", min_sup_percentage=5)
 
+    print(str(sfd_list['1']))
