ae74d56c5ae132582c24b0dd22b90b395b201353
Dhiraj Bag <dhirajbag.db@gmail.com>
2023-05-30 23:13:28 +0530

    Updated call to process() in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from processor import Processor
 import pandas as pd
 
 processor = Processor()
-processor.set_input_dataset('./input/netflix/',
+processor.set_input_dataset('../input/netflix/',
                             'netflix_dataset_sample.csv',
                             oid_attribute = 'uid',
                             item_list_attribute = 'movie_ids',
@@ -10,13 +10,14 @@ processor.set_input_dataset('./input/netflix/',
 
 processor.set_minmum_support_count(2)
 
-df = pd.read_csv('./input/netflix/Netflix_Dataset_Movie.csv', dtype=str)
+df = pd.read_csv('../input/netflix/Netflix_Dataset_Movie.csv', dtype=str)
 mid_to_name = {}
 for i in range(len(df)):
     mid_to_name[int(df.iloc[i]['Movie_ID'])] = df.iloc[i]['Name']
 
 
-processor.process(produce_intermediate_imgs=True,
+processor.process(min_support_count=2,
+                  produce_intermediate_imgs=True,
                     produce_final_img=True,
-                    custom_name_mapping=mid_to_name,
-                    dtype_key=int)
+                     custom_name_mapping=mid_to_name,
+                      dtype_key=int)
