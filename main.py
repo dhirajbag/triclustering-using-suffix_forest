3eb209957ce40fae0188d98b065bdce58dd8818f
Dhiraj Bag <dhirajbag.db@gmail.com>
2023-05-30 12:41:51 +0530

    Sample script to use the Processor class

---
+++ main.py
@@ -0,0 +1,22 @@
+from processor import Processor
+import pandas as pd
+
+processor = Processor()
+processor.set_input_dataset('./input/netflix/',
+                            'netflix_dataset_sample.csv',
+                            oid_attribute = 'uid',
+                            item_list_attribute = 'movie_ids',
+                            split_attribute = 'year')
+
+processor.set_minmum_support_count(2)
+
+df = pd.read_csv('./input/netflix/Netflix_Dataset_Movie.csv', dtype=str)
+mid_to_name = {}
+for i in range(len(df)):
+    mid_to_name[int(df.iloc[i]['Movie_ID'])] = df.iloc[i]['Name']
+
+
+processor.process(produce_intermediate_imgs=True,
+                    produce_final_img=True,
+                    custom_name_mapping=mid_to_name,
+                    dtype_key=int)
